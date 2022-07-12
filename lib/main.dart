54f3dc26bab9172d598f7970ede7f3e0be442c16
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-07-12 14:00:17 -0300

    feat: add promotion entity

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vinicius_eng_gruposbf_mobile/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SBF Challenge',
-      home: Container(),
-    );
-  }
+  runApp(const App());
 }
