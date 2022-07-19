18a9902e243cd64c26a3b03b15823f32537af82e
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-07-19 19:05:08 -0300

    feat: add style

---
+++ lib/presentation/style/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color redBackground = Color(0xFFE30000);
+  static const Color greyHintText = Color(0xFF737373);
+}
---
+++ lib/presentation/style/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static String centauroLogoWhite = 'assets/images/centauro_logo_white.svg';
+  static String centauroLogoBlack = 'assets/images/centauro_logo_black.svg';
+  static String cartIcon = 'assets/icons/cart_icon.svg';
+  static String searchIcon = 'assets/icons/search_icon.svg';
+  static String promotionBanner = 'assets/images/promotion_banner.png';
+}
