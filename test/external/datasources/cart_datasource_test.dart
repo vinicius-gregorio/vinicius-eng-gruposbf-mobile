d3cdd3424bfffc72da7df88d88fe83741fd2717c
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-07-20 23:40:52 -0300

    feat: add addToCart datasource tests

--- test/external/datasources/cart_datasource_test.dart
+++ test/external/datasources/cart_datasource_test.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:vinicius_eng_gruposbf_mobile/config/app_log.dart';
 import 'package:vinicius_eng_gruposbf_mobile/domain/usecases/cart/add_to_cart_usecase.dart';
 import 'package:vinicius_eng_gruposbf_mobile/external/datasources/cart_datasource.dart';
 import 'package:vinicius_eng_gruposbf_mobile/infra/repositories/cart_repository.dart';
@@ -11,7 +13,16 @@ void main() async {
   final usecase =
       AddToCartUsecaseImpl(CartRepositoryImpl(CartDatasourceImpl(dio, sharedPreferences)));
 
-  test('should add to cart', () {
+  test('should completes usecase', () {
     expect(usecase('1'), completes);
+  });
+
+  test('should add to cart', () async {
+    List<String>? cart = sharedPreferences.getStringList('cart');
+    String? id = cart?.first;
+    appLog(id.toString());
+    expect(id, '1');
+    expect(id, isA<String>());
+    expect(cart, isA<List<String>>());
   });
 }
