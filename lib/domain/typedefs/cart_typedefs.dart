7aef0d65ac38f5d1a678c846f73d34779037fcdf
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-07-21 22:47:10 -0300

    refact: refact cartTypeDef by adding response as answer

--- lib/domain/typedefs/cart_typedefs.dart
+++ lib/domain/typedefs/cart_typedefs.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
-import 'package:vinicius_eng_gruposbf_mobile/domain/entities/cart_item.dart';
+import 'package:dio/dio.dart';
 import 'package:vinicius_eng_gruposbf_mobile/domain/errors/cart_error.dart';
 
 typedef GetCartCall = Future<Either<CartError, List<String>>>;
-typedef CheckoutCartCall = Future<Either<CartError, dynamic>>;
+typedef CheckoutCartCall = Future<Either<CartError, Response>>;
